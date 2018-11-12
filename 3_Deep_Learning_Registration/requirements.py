e50a4142d17aa6210ffea9d10b9353cfdf88971f
Natandrade <natandrade@users.noreply.github.com>
2018-11-11 22:10:58 -0200

    Update and rename test to requirements.py

---
+++ 3_Deep_Learning_Registration/requirements.py
@@ -0,0 +1,26 @@
+#For install virtualenv: pip install -r requirements.txt
+
+pip install tensorflow
+pip install tensorflow-gpu
+pip install keras
+pip install dltk
+pip install niftynet
+pip install SimpleITK
+pip install sympy
+pip install nibabel 
+pip install pydicom 
+pip install medpy
+pip install -U scikit-learn
+pip install pydot
+pip install graphviz
+
+
+
+
+
+
+
+
+
+
+
